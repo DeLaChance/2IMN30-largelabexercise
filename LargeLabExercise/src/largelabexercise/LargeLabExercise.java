bfa22fc952cb231a9237ffa003c2f31c4d3f5dbc
Lucien <ldjvalstar@gmail.com>
2015-09-29 10:26:08 +0200

    Setting up project.

---
+++ LargeLabExercise/src/largelabexercise/LargeLabExercise.java
@@ -0,0 +1,17 @@
+package largelabexercise;
+
+/**
+ *
+ * @author lucien
+ */
+public class LargeLabExercise {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+
+    }
+    
+}
