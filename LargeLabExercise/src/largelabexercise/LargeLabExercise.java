cf1ff63645d2567f26050713c9bb5ac667634414
Lucien <ldjvalstar@gmail.com>
2015-10-01 09:21:11 +0200

    Testing the java project on VM. Added documentation part.

--- LargeLabExercise/src/largelabexercise/LargeLabExercise.java
+++ LargeLabExercise/src/largelabexercise/LargeLabExercise.java
@@ -1,5 +1,9 @@
 package largelabexercise;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import magick.MagickException;
+
 /**
  *
  * @author lucien
@@ -11,7 +15,19 @@ public class LargeLabExercise {
      */
     public static void main(String[] args) 
     {
-        // abc
+        System.out.println("hello world!");
+        
+//        try {
+//            Image im = new Image("/home/lucien/Documents/Studies/Year 5/Q1/2IMN30/pictures/40bd6a7e-2809-4008-82da-d3c68a07f1ae.jpg");
+//            long start = System.currentTimeMillis();
+//            im.processImage();
+//            long end = System.currentTimeMillis();
+//            long delta = end - start;
+//            System.out.println(im.getFileSize() + " : " + delta);
+//            im.write("/home/lucien/Downloads/iconjpg.jpg");
+//        } catch (MagickException ex) {
+//            Logger.getLogger(LargeLabExercise.class.getName()).log(Level.SEVERE, null, ex);
+//        }
     }
     
 }
