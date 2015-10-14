f6e5ce44e1086271a3c9b22bcdc92e66bc0365c1
Lucien <ldjvalstar@gmail.com>
2015-10-14 10:29:17 +0200

    Test added for estimating load again. Images added.

--- LargeLabExercise/src/gen/LargeLabExercise.java
+++ LargeLabExercise/src/gen/LargeLabExercise.java
@@ -7,6 +7,7 @@ import magick.MagickException;
 import master.JobPicker;
 import both.Monitor;
 import both.NetworkThread;
+import java.io.File;
 import master.Master;
 import master.Scheduler;
 import slave.Slave;
@@ -20,21 +21,56 @@ public class LargeLabExercise {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) 
-    {
-        if( args[0].equals("slave") )
+//    public static void main(String[] args) 
+//    {
+//        if( args[0].equals("slave") )
+//        {
+//            // Need to start monitor, processor
+//            Slave slave = new Slave();
+//            slave.run();
+//        }
+//        else 
+//        {
+//            Master master = Master.getInstance();
+//            master.run();
+//            System.exit(0);
+//        }
+//
+//    }
+    
+    public static void main(String[] args) {
+        if( args.length != 3)
         {
-            // Need to start monitor, processor
-            Slave slave = new Slave();
-            slave.run();
+            System.out.println("Too few args -- needs input, output and logging folder");
+            System.exit(1);
         }
-        else 
+        
+        File folder = new File(args[0]);
+        File outputFolder = new File(args[1]);
+        Logging logging = Logging.getInstance();
+        logging.setInputDir(args[2]);
+        
+        if( folder.exists() && outputFolder.exists() )
         {
-            Master master = Master.getInstance();
-            master.run();
-            System.exit(0);
+            // List all files in input folder args[0]
+            File[] allFiles = folder.listFiles();
+            for(File f : allFiles)
+            {
+                try {
+                    // Loop over the files
+                    String fileName = f.getName();
+                    System.out.println("Processing: " + f.getName());
+                    
+                    logging.addJobToLog(fileName, Logging.RUNNING_JOB);
+                    Image image = new Image(f);
+                    image.processImage();
+                    image.write(args[1] + "out_" + fileName);
+                    logging.addJobToLog(fileName, Logging.COMPLETED_JOB);
+                } catch (MagickException ex) {
+                    Logger.getLogger(JobEstimator.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
         }
-
-    }
+    }    
     
 }
